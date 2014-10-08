ff00208d33d9c625322f87958bd739479e941ceb
kevanescence <kevanescence@hotmail.fr>
2014-10-08 15:27:59 +0200

    Update the sql file

--- public_html/lab1.sql
+++ public_html/lab1.sql
@@ -37,41 +37,101 @@ CONSTRAINT pk_recommends PRIMARY KEY (recommender, recommended)
 );
 DELETE FROM Topics;	
 DELETE FROM Experts;
+
 INSERT INTO Experts(name,email) 
-VALUES ('Jean-Michel MARTIN', 'jmm@gmail.com'),
+VALUES  ('Elsa VERSAIRE', 'elsav@outlook.com'), -- -8
+		('Rose VERSAIRE', 'rosev@outlook.com'),
+		('Laure VERSAIRE', 'laurev@outlook.com'),
+		('Annie VERSAIRE', 'anniev@outlook.com'),
+		('Jean-Michel MARTIN', 'jmm@gmail.com'), --4
 		('Jean-Jacques De Compostelle', 'jjc@hotmail.com'),
-		('Rémy MARTIN', 'rm@yahoo.fr'),
+		('RÃ©my MARTIN', 'rm@yahoo.fr'),
 		('Bernard DUPONT', 'lebogossedu38@gmail.com'),
 		('Yvon ANCHIER', 'ya@outlook.com');
 		
+		
 	
 INSERT INTO Topics(name, topic_description, originator)
-VALUES ('SQL', 'Language for data bases', IDENT_CURRENT( 'Experts' ) - 4 ),
+VALUES  ('Management' , 'Learn to lead a team', IDENT_CURRENT( 'Experts' ) - 6), -- -13
+		('Human science' , 'All HS fields', IDENT_CURRENT( 'Experts' ) - 7),
+		('Psychology' , 'Learn how people minds work', IDENT_CURRENT( 'Experts' ) - 6),
+		('Communication' , 'How to solve communication issues', IDENT_CURRENT( 'Experts' ) - 8),-- -10
+		('Advertising' , 'Learn how to drive people into buying our product', IDENT_CURRENT( 'Experts' ) - 6), 
+		('Marketing' , 'How people can have a good image of our product', IDENT_CURRENT( 'Experts' ) - 7),
+		('SQL', 'Language for data bases', IDENT_CURRENT( 'Experts' ) - 4 ),
+		('Algorithm', 'How algorithm work', IDENT_CURRENT( 'Experts' ) - 2 ),
+		('Data structures', 'Basic data structures', IDENT_CURRENT( 'Experts' ) - 2 ), -- -5
+		('Parallel programming', 'Parallel algorithms', IDENT_CURRENT( 'Experts' ) - 1 ), 
 		('DB', 'All data bases technologies',IDENT_CURRENT( 'Experts' ) - 4 ),
 		('CS', 'Computer science' ,IDENT_CURRENT( 'Experts' ) - 2),
 		('UML', 'Unified modeling language', IDENT_CURRENT( 'Experts' ) - 1),
 		('Software engineering', 'Software engineering', IDENT_CURRENT( 'Experts' ));
-		
+
+delete from Expert_in;
+-- topic's originator is an expert of this topic
+INSERT INTO Expert_in (topic_id, expert_id)
+VALUES		(IDENT_CURRENT( 'Topics' ) - 13, IDENT_CURRENT( 'Experts' ) - 6),
+		(IDENT_CURRENT( 'Topics' ) - 12, IDENT_CURRENT( 'Experts' ) - 7),
+		(IDENT_CURRENT( 'Topics' ) - 11, IDENT_CURRENT( 'Experts' ) - 6),
+		(IDENT_CURRENT( 'Topics' ) - 10, IDENT_CURRENT( 'Experts' ) - 8),
+		(IDENT_CURRENT( 'Topics' ) - 9, IDENT_CURRENT( 'Experts' ) - 6),
+		(IDENT_CURRENT( 'Topics' ) - 8, IDENT_CURRENT( 'Experts' ) - 7),
+		(IDENT_CURRENT( 'Topics' ) - 7, IDENT_CURRENT( 'Experts' ) - 4),
+		(IDENT_CURRENT( 'Topics' ) - 6, IDENT_CURRENT( 'Experts' ) - 2),
+		(IDENT_CURRENT( 'Topics' ) - 5, IDENT_CURRENT( 'Experts' ) - 2),
+		(IDENT_CURRENT( 'Topics' ) - 4, IDENT_CURRENT( 'Experts' ) - 1),
+		(IDENT_CURRENT( 'Topics' ) - 3, IDENT_CURRENT( 'Experts' ) - 4),
+		(IDENT_CURRENT( 'Topics' ) - 2, IDENT_CURRENT( 'Experts' ) - 2),
+		(IDENT_CURRENT( 'Topics' ) - 1, IDENT_CURRENT( 'Experts' ) - 1),
+		(IDENT_CURRENT( 'Topics' ), IDENT_CURRENT( 'Experts' ));
+
+-- aditional experts association
 INSERT INTO Expert_in (expert_id, topic_id)
-VALUES (IDENT_CURRENT( 'Experts' ) - 4, IDENT_CURRENT( 'Topics' )-4),
-		(IDENT_CURRENT( 'Experts' ) - 4, IDENT_CURRENT( 'Topics' )-3),
-		(IDENT_CURRENT( 'Experts' ) - 3, IDENT_CURRENT( 'Topics' )-2),
-		(IDENT_CURRENT( 'Experts' ), IDENT_CURRENT( 'Topics' )-1),
-		(IDENT_CURRENT( 'Experts' ), IDENT_CURRENT( 'Topics' ));
-		
+VALUES  (IDENT_CURRENT( 'Experts' ) - 8, IDENT_CURRENT( 'Topics' )-13),
+		(IDENT_CURRENT( 'Experts' ) - 8, IDENT_CURRENT( 'Topics' )-11),
+		(IDENT_CURRENT( 'Experts' ) - 5, IDENT_CURRENT( 'Topics' )-10),
+		(IDENT_CURRENT( 'Experts' ) - 3, IDENT_CURRENT( 'Topics' )-1),
+		(IDENT_CURRENT( 'Experts' ) - 2, IDENT_CURRENT( 'Topics' )-4),
+		(IDENT_CURRENT( 'Experts' ) - 1, IDENT_CURRENT( 'Topics' )-2),
+		(IDENT_CURRENT( 'Experts' ), IDENT_CURRENT( 'Topics' )-1);
+
+
 INSERT INTO Subtopics(parent_id, child_id)
-VALUES (IDENT_CURRENT( 'Topics') -3, IDENT_CURRENT( 'Topics' )-4),
+VALUES (IDENT_CURRENT( 'Topics') -12, IDENT_CURRENT( 'Topics' )-13),
+	   (IDENT_CURRENT( 'Topics') -11, IDENT_CURRENT( 'Topics' )-13),
+	   (IDENT_CURRENT( 'Topics') -12, IDENT_CURRENT( 'Topics' )-11),
+	   (IDENT_CURRENT( 'Topics') -12, IDENT_CURRENT( 'Topics' )-10),
+	   (IDENT_CURRENT( 'Topics') -8, IDENT_CURRENT( 'Topics' )-9),
+	   (IDENT_CURRENT( 'Topics') -2, IDENT_CURRENT( 'Topics' )-7),
+	   (IDENT_CURRENT( 'Topics') -3, IDENT_CURRENT( 'Topics' )-7),
+	   (IDENT_CURRENT( 'Topics') -2, IDENT_CURRENT( 'Topics' )-8),
+	   
+	   (IDENT_CURRENT( 'Topics') -6, IDENT_CURRENT( 'Topics' )-5),
+	   (IDENT_CURRENT( 'Topics') -2, IDENT_CURRENT( 'Topics' )-5),
+	   
+	   (IDENT_CURRENT( 'Topics') -5, IDENT_CURRENT( 'Topics' )-4),
+	   (IDENT_CURRENT( 'Topics') -2, IDENT_CURRENT( 'Topics' )-4),
+	   
 		(IDENT_CURRENT( 'Topics' )-2, IDENT_CURRENT( 'Topics' ) - 3),
-		(IDENT_CURRENT( 'Topics' ), IDENT_CURRENT( 'Topics' ) - 1),
-		(IDENT_CURRENT( 'Topics') -2, IDENT_CURRENT( 'Topics' ));
+		(IDENT_CURRENT( 'Topics') -2, IDENT_CURRENT( 'Topics' ) - 1 ),
+		(IDENT_CURRENT( 'Topics'), IDENT_CURRENT( 'Topics' ) - 1 ),
+		(IDENT_CURRENT( 'Topics') - 2, IDENT_CURRENT( 'Topics' ));
 		
+delete from Recommends;
 INSERT INTO Recommends(recommender, recommended)
-VALUES (IDENT_CURRENT( 'Experts' ) - 4, IDENT_CURRENT( 'Experts' ) - 3),
-		(IDENT_CURRENT( 'Experts' ) - 2, IDENT_CURRENT( 'Experts' ) - 3),
+VALUES (IDENT_CURRENT( 'Experts' ) - 8, IDENT_CURRENT( 'Experts' ) - 3),
+		(IDENT_CURRENT( 'Experts' ) - 7, IDENT_CURRENT( 'Experts' ) - 3),
+		(IDENT_CURRENT( 'Experts' ) - 6, IDENT_CURRENT( 'Experts' ) - 1),
+		(IDENT_CURRENT( 'Experts' ) - 7, IDENT_CURRENT( 'Experts' ) - 1),
+		(IDENT_CURRENT( 'Experts' ) - 4, IDENT_CURRENT( 'Experts' ) - 1),
 		(IDENT_CURRENT( 'Experts' ) - 3, IDENT_CURRENT( 'Experts' ) - 1);
-
 		
+INSERT INTO Recommends(recommender, recommended, comment)
+VALUES  (IDENT_CURRENT( 'Experts' ) - 4, IDENT_CURRENT( 'Experts' ) - 7, 'never late' ),
+		(IDENT_CURRENT( 'Experts' ) - 2, IDENT_CURRENT( 'Experts' ) - 6, 'serious at job'),
+		(IDENT_CURRENT( 'Experts' ) - 3, IDENT_CURRENT( 'Experts' ) - 6, 'has lot of experience'),
+		(IDENT_CURRENT( 'Experts' ) - 3, IDENT_CURRENT( 'Experts' ) - 5, 'know to learn people'),
+		(IDENT_CURRENT( 'Experts' ) - 3, IDENT_CURRENT( 'Experts' ) - 2, 'a cool comment');
 		
-		
-
-
+											
+select  topic_id, expert_id from Expert_in order by topic_id ;
